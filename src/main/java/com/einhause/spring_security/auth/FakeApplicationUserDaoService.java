d022ad8b7bfd307c5dd5a8945acb35a4c2fc21e1
Eric Einhaus <eeinhaus17@gmail.com>
2022-03-25 09:52:26 -0400

    Switched in memory database users to users from the 'fake' repository

---
+++ src/main/java/com/einhause/spring_security/auth/FakeApplicationUserDaoService.java
@@ -0,0 +1,63 @@
+package com.einhause.spring_security.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.einhause.spring_security.security.ApplicationUserRole.*;
+
+@Repository("fake")
+public class FakeApplicationUserDaoService  implements ApplicationUserDao {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public FakeApplicationUserDaoService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
+        return getApplicationUsers()
+                .stream()
+                .filter(applicationUser -> username.equals(applicationUser.getUsername()))
+                .findFirst();
+    }
+
+    private List<ApplicationUser> getApplicationUsers() {
+        List<ApplicationUser> applicationUsers = List.of(
+                new ApplicationUser(
+                        "anna",
+                        passwordEncoder.encode("password123"),
+                        STUDENT.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        "linda",
+                        passwordEncoder.encode("password123"),
+                        ADMIN.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        "tom",
+                        passwordEncoder.encode("password123"),
+                        ADMIN_TRAINEE.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+        return applicationUsers;
+    }
+}
