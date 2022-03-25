0ef34a5d452bce7a9cc337e9809bd101b32f401f
Eric Einhaus <eeinhaus17@gmail.com>
2022-03-25 16:14:07 -0400

    Added successful auth method to generate a new JWT token and add to res

--- src/main/java/com/einhause/spring_security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/einhause/spring_security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,15 +1,22 @@
 package com.einhause.spring_security.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -29,7 +36,7 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
                     new ObjectMapper().readValue(request.getInputStream(),
                             UsernameAndPasswordAuthenticationRequest.class);
 
-            // authentication check, checks username and password in JWT to see if it is a match
+            // authentication check, checks username and password in body to see if it is a match
             Authentication authentication = new UsernamePasswordAuthenticationToken(
                     authenticationRequest.getUsername(),
                     authenticationRequest.getPassword()
@@ -44,5 +51,27 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
             // invalid credentials
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        // successful authentication logic, will run only if authenticated username and password
+        // encrypt key
+        String key = "supersecretkeysupersecretkeysupersecretkeysupersecretkeysupersecretkeysupersecretkey";
+
+        // token generation
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+                .compact();
+
+        // add JWT to response
+        response.addHeader("Authorization", String.format("Bearer %s", token));
     }
 }
